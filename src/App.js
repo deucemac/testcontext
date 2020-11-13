770566b585c942a7d947580faac65e89f1c98b01
Dwayne McDowell <dwaynemcdowell@Dwaynes-MacBook-Pro.local>
2020-11-12 23:01:47 -0500

    useCallback

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import React from 'react'
+import React, { useState, useCallback } from 'react'
 import FunctionContextComponent from './FunctionContextComponent'
 import { ThemeProvider } from './ThemeContext'
+import List from './List'
 
 export default function App() {
+  const [number, setNumber] = useState(1)
+  const [dark, setDark] = useState(false)
+
+  const getItems = useCallback(() => {
+    return [number, number + 1, number + 2]
+  }, [number])
+
+  const theme = {
+    backgroundColor: dark ? '#333' : '#FFF',
+    color: dark ? '#FFF' : '#333'
+  }
+
   return (
-    <ThemeProvider>
-      <FunctionContextComponent />
-    </ThemeProvider>
+    // <ThemeProvider>
+    //   <FunctionContextComponent />
+    // </ThemeProvider>
+    <div style={theme}>
+      <input
+        type="number"
+        value={number}
+        onChange={e => setNumber(parseInt(e.target.value))}
+      />
+      <button onClick={()=> setDark(prevDark => !prevDark)}>
+        Toggle Theme
+      </button>
+      <List getItems={getItems} />
+    </div>
   )
 }
 
